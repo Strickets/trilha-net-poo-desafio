e68f0d77bf69fdecc324c4932264e7d1fa965780
Diogo Moro <137415220+Strickets@users.noreply.github.com>
2023-07-30 16:14:21 -0300

    Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,22 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+        public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        private string Modelo {get; set;}
 
-        public Smartphone(string numero)
+        private string IMEI {get; set;}
+
+        private int Memoria {get; set;}
+
+        public Smartphone(string numero, string imie, string modelo, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imie;
+            Memoria = memoria;
+                // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
